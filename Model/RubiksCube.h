fd43073dd23867d229f37d4464c49988afd5d782
UniteAman <129004507+UniteAman@users.noreply.github.com>
2024-11-24 10:41:23 +0530

    Update RubiksCube.h

--- Model/RubiksCube.h
+++ Model/RubiksCube.h
@@ -5,12 +5,7 @@
 
 using namespace std;
 
-/**
- * A base class for all Rubik's Cube Model. There are various representation for Rubik's Cube.
- * Each one has it's own special ways of definitions. This class provides a shared functionality
- * between all models.
- * We'll benchmark all models and observe which one is better for performance.
- */
+
 
 class RubiksCube {
 public:
